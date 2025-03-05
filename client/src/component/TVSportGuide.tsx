d777d4b96bc8beb3479b5c56c5ce2b80cf12131b
Timothy Pharo <timothy.pharo@gmail.cim>
2025-03-05 14:30:25 +0000

    Update sports

--- client/src/component/TVSportGuide.tsx
+++ client/src/component/TVSportGuide.tsx
@@ -18,7 +18,7 @@ const TVSportGuide: Component = () => {
                     {/*        typeof e.data == "number" && e.data > 100) document.getElementById("outputFrame").height = e.data + "px";}},false);*/}
                     {/*</script>*/}
                     <iframe className="frameData" id="outputFrame"
-                            src="https://sport-tv-guide.live/sportwidget/ed8dac5c0a64?time_zone=Europe%2FLondon&fc=2&time12=0&sports=18,1,32,7,39,40,12&bg=f8f8f9&bgs=b7b7b7&grp=1&sd=0&lng=1"
+                            src="https://sport-tv-guide.live/sportwidget/ed8dac5c0a64?time_zone=Europe%2FLondon&fc=2&time12=0&sports=15,39,40,50,16,4&bg=f8f8f9&bgs=b7b7b7&grp=1&sd=0&lng=1"
                             style="position:relative;border:none;height:40em;width: 35em;" frameBorder="0"></iframe>
                     <div style="padding:5px;text-align:center;font-size:10px">Powered by <a
                         href="https://sport-tv-guide.live">Live
